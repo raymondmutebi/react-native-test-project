df8fcd5e9c6cfd0e9ed01514ee0737b9d9e6e646
Raymond Mutebi <raymond@pahappa.com>
2022-05-18 20:10:07 +0300

    firt demo

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
+  Button,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text cllicked");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello Ray! Really longer text to see what realy happens in.
+      </Text>
+      <Button
+        title="Alert button"
+        style={myCustomStyles}
+        onPress={() =>
+          Alert.alert("You are hacked  ", "Proceed?", [
+            { text: "Yes", onPress: () => console.log("He said yes") },
+            { text: "No", onPress: () => console.log("He said no") },
+          ])
+        }
+      />
+
+      <Button
+        title="Prompt button"
+        color={"blue"}
+        onPress={() =>
+          Alert.prompt("You are hacked  ", "Proceed?", () =>
+            console.log("He said yes")
+          )
+        }
+      />
+
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+        <Image
+          blurRadius={5}
+          fadeDuration={5000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
+    </SafeAreaView>
   );
 }
 
+const myCustomStyles = { backgroundColor: "orange", color: "brown" };
+
+//StyleSheet.create helps us to validate the props passed. Though we can just use plain js objects
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "purple",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
